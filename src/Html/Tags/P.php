87ce09473154309800f622cde1ee8c213903bd5e
Max Zheltikov <maxcruer@gmail.com>
2021-06-09 18:45:06 +0300

    getChildrenDeclaration

--- src/Html/Tags/P.php
+++ src/Html/Tags/P.php
@@ -3,23 +3,24 @@
 namespace Zheltikov\PhpXhp\Html\Tags;
 
 use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
 use Zheltikov\PhpXhp\Html\Category\Flow;
 use Zheltikov\PhpXhp\Html\Category\Palpable;
 use Zheltikov\PhpXhp\Html\Element;
 
 class P extends Element implements Flow, Palpable
 {
-    // use Validation;
+    use Validation;
 
-    /* protected static function getChildrenDeclaration(): XHPChild\Constraint
+    protected static function getChildrenDeclaration(): Constraint
     {
-        return XHPChild\any_number_of(
+        /* return XHPChild\any_number_of(
             XHPChild\any_of(
                 XHPChild\pcdata(),
                 XHPChild\of_type<Category\Phrase>(),
             ),
-        );
-    } */
+        ); */
+    }
 
     protected string $tagName = 'p';
 }
