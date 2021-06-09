3ead7e70771b8f6500e1689063aca78e780d8bfc
Max Zheltikov <maxcruer@gmail.com>
2021-06-09 15:20:54 +0300

    bulk add html attributes

--- src/Html/Element.php
+++ src/Html/Element.php
@@ -57,8 +57,94 @@ abstract class Element extends Primitive
     protected static function __xhpAttributeDeclaration(): array
     {
         return [
+            // Global HTML attributes
+            'accesskey' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'autocapitalize' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'off', // identical to none
+                    'none',
+                    'on', // identical to sentences
+                    'sentences',
+                    'words',
+                    'characters',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'autofocus' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
             'class' => [
                 XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'contenteditable' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'true',
+                    'false',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'contextmenu' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'dir' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'auto',
+                    'ltr',
+                    'rtl',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'draggable' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'true',
+                    'false',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'dropzone' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'enterkeyhint' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'enter',
+                    'done',
+                    'go',
+                    'next',
+                    'previous',
+                    'search',
+                    'send',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'hidden' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
                 null, // extraType
                 null, // defaultValue
                 false, // required
@@ -69,6 +155,196 @@ abstract class Element extends Primitive
                 null, // defaultValue
                 false, // required
             ],
+            'is' => [ // needs to be a name of a defined custom element
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'inert' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'inputmode' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'decimal',
+                    'email',
+                    'full-width-latin',
+                    'kana',
+                    'katakana',
+                    'latin',
+                    'latin-name',
+                    'latin-prose',
+                    'none',
+                    'numeric',
+                    'search',
+                    'tel',
+                    'text',
+                    'url',
+                    'verbatim',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'itemid' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'itemprop' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'itemref' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'itemscope' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'itemtype' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'lang' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'nonce' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'role' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'spellcheck' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'true',
+                    'false',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'style' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'tabindex' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'title' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'translate' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'yes',
+                    'no',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+
+            // Javascript events
+            /*
+            string onabort,
+            string onauxclick,
+            string onblur,
+            string oncancel,
+            string oncanplay,
+            string oncanplaythrough,
+            string onchange,
+            string onclick,
+            string onclose,
+            string oncontextmenu,
+            string oncopy,
+            string oncuechange,
+            string oncut,
+            string ondblclick,
+            string ondrag,
+            string ondragend,
+            string ondragenter,
+            string ondragexit,
+            string ondragleave,
+            string ondragover,
+            string ondragstart,
+            string ondrop,
+            string ondurationchange,
+            string onemptied,
+            string onended,
+            string onerror,
+            string onfocus,
+            string onformdata,
+            string oninput,
+            string oninvalid,
+            string onkeydown,
+            string onkeypress,
+            string onkeyup,
+            string onload,
+            string onloadeddata,
+            string onloadedmetadata,
+            string onloadstart,
+            string onmousedown,
+            string onmouseenter,
+            string onmouseleave,
+            string onmousemove,
+            string onmouseout,
+            string onmouseover,
+            string onmouseup,
+            string onmousewheel,
+            string onpaste,
+            string onpause,
+            string onplay,
+            string onplaying,
+            string onprogress,
+            string onratechange,
+            string onreset,
+            string onresize,
+            string onscroll,
+            string onsecuritypolicyviolation,
+            string onseeked,
+            string onseeking,
+            string onselect,
+            string onshow,
+            string onslotchange,
+            string onstalled,
+            string onsubmit,
+            string onsuspend,
+            string ontimeupdate,
+            string ontoggle,
+            string onvolumechange,
+            string onwaiting,
+            string onwheel,
+            */
         ];
     }
 }
