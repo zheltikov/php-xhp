3de26af943f2db66e6d797e8a802f1e786ac5f91
Max Zheltikov <maxcruer@gmail.com>
2021-06-09 17:58:05 +0300

    .

--- src/Html/Tags/P.php
+++ src/Html/Tags/P.php
@@ -6,5 +6,17 @@ use Zheltikov\PhpXhp\Html\Element;
 
 class P extends Element
 {
+    // use XHPChild\Validation;
+
+    /* protected static function getChildrenDeclaration(): XHPChild\Constraint
+    {
+        return XHPChild\any_number_of(
+            XHPChild\any_of(
+                XHPChild\pcdata(),
+                XHPChild\of_type<Category\Phrase>(),
+            ),
+        );
+    } */
+
     protected string $tagName = 'p';
 }
