054127ac7149ac0b57b36b858764ea1bb10ef191
Max Zheltikov <maxcruer@gmail.com>
2021-06-10 17:51:10 +0300

    Html\Tags\Base

---
+++ src/Html/Tags/Base.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Html\Category\Metadata;
+use Zheltikov\PhpXhp\Html\Singleton;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+final class Base extends Singleton implements Metadata
+{
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'href' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'target' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    protected string $tagName = 'base';
+}
