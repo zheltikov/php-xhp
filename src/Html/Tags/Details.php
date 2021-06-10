4c488383682f84d4d167f0cef067a2b4721e611d
Max Zheltikov <maxcruer@gmail.com>
2021-06-10 20:21:25 +0300

    Html\Tags\Details

---
+++ src/Html/Tags/Details.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
+use Zheltikov\PhpXhp\Core\ChildValidation;
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Interactive;
+use Zheltikov\PhpXhp\Html\Category\Palpable;
+use Zheltikov\PhpXhp\Html\Category\Sectioning;
+use Zheltikov\PhpXhp\Html\Element;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+final class Details extends Element implements Flow, Interactive, Sectioning, Palpable
+{
+    use Validation;
+
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'open' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    protected static function getChildrenDeclaration(): Constraint
+    {
+        return ChildValidation::sequence(
+            ChildValidation::of_type(Summary::class),
+            ChildValidation::at_least_one_of(
+                ChildValidation::of_type(Flow::class)
+            )
+        );
+    }
+
+    protected string $tagName = 'details';
+}
