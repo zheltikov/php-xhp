0078491eeeb896bcae0aa1ace9d8bc1e37197ce9
Max Zheltikov <maxcruer@gmail.com>
2021-06-10 20:18:13 +0300

    Html\Tags\Del

---
+++ src/Html/Tags/Del.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
+use Zheltikov\PhpXhp\Core\ChildValidation;
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Phrase;
+use Zheltikov\PhpXhp\Html\Element;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+final class Del extends Element implements Phrase, Flow
+{
+    use Validation;
+
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'cite' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'datetime' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    // transparent
+    protected static function getChildrenDeclaration(): Constraint
+    {
+        return ChildValidation::any_number_of(
+            ChildValidation::any_of(
+                ChildValidation::pcdata(),
+                ChildValidation::of_type(Flow::class)
+            )
+        );
+    }
+
+    protected string $tagName = 'del';
+}
