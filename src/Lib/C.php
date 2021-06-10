b1037e8e829135519ae90f9999f75a2fedf51493
Max Zheltikov <maxcruer@gmail.com>
2021-06-10 10:43:31 +0300

    C::onlyx

--- src/Lib/C.php
+++ src/Lib/C.php
@@ -24,4 +24,50 @@ class C
     {
         return static::count($container) === 0;
     }
+
+    /**
+     * Returns the first and only element of the given Traversable, or throws if the
+     * Traversable is empty or contains more than one element.
+     *
+     * An optional format string (and format arguments) may be passed to specify
+     * a custom message for the exception in the error case.
+     *
+     * For Traversables with more than one element, see `C::firstx`.
+     *
+     * @param array $container
+     * @param string|null $format_string
+     * @param mixed ...$format_args
+     * @return mixed
+     * @throws \Exception
+     */
+    function onlyx(array $container, ?string $format_string = null, ...$format_args) // : mixed
+    {
+        $first = true;
+        $result = null;
+        foreach ($container as $value) {
+            Assert::invariant(
+                $first,
+                '%s',
+                $format_string === null
+                    ? Str::format(
+                    'Expected exactly one element%s.',
+                    \is_iterable($container)
+                        ? ' but got ' . static::count($container)
+                        : '',
+                )
+                    : \vsprintf($format_string, $format_args),
+            );
+            $result = $value;
+            $first = false;
+        }
+        Assert::invariant(
+            $first === false,
+            '%s',
+            $format_string === null
+                ? 'Expected non-empty Traversable.'
+                : \vsprintf($format_string, $format_args),
+        );
+        /* HH_FIXME[4110] $first is false implies $result is set to T */
+        return $result;
+    }
 }
