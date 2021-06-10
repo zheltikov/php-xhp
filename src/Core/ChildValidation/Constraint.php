dfd6852744dfef2229221aec6448dca128fc1af9
Max Zheltikov <maxcruer@gmail.com>
2021-06-10 18:07:48 +0300

    ups

--- src/Core/ChildValidation/Constraint.php
+++ src/Core/ChildValidation/Constraint.php
@@ -12,5 +12,5 @@ interface Constraint
     /**
      * @return ?array
      */
-    public function legacySerializeAsLeaf(): ?array; // : ?(LegacyConstraintType, mixed);
+    public function legacySerializeAsLeaf(); // : ?array; // : ?(LegacyConstraintType, mixed);
 }
