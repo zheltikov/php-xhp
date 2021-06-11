3fd8f5b149ab4918d12422b86da303059f27f931
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 13:48:20 +0300

    Html\Tags\Li

---
+++ src/Html/Tags/Li.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
+use Zheltikov\PhpXhp\Core\ChildValidation;
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Element;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+final class Li extends Element
+{
+    use Validation;
+
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'value' => [
+                XHPAttributeType::TYPE_INTEGER()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    protected static function getChildrenDeclaration(): Constraint
+    {
+        return ChildValidation::any_number_of(
+            ChildValidation::any_of(
+                ChildValidation::pcdata(),
+                ChildValidation::of_type(Flow::class)
+            )
+        );
+    }
+
+    protected string $tagName = 'li';
+}
