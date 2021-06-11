f6d16cce9416da8a7de48b5a53ecf4bfe10303ba
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 12:52:51 +0300

    Html\Tags\Img

---
+++ src/Html/Tags/Img.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Html\Category\Embedded;
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Interactive;
+use Zheltikov\PhpXhp\Html\Category\Palpable;
+use Zheltikov\PhpXhp\Html\Category\Phrase;
+use Zheltikov\PhpXhp\Html\Singleton;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+// Iff Interactive when usemap is set
+final class Img extends Singleton implements Phrase, Flow, Embedded, Palpable, Interactive
+{
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'alt' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'crossorigin' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [// extraType
+                    'anonymous',
+                    'use-credentials',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'decoding' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [// extraType
+                    'async',
+                    'auto',
+                    'sync',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'height' => [
+                XHPAttributeType::TYPE_INTEGER()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'ismap' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'loading' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [// extraType
+                    'eager',
+                    'lazy',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'referrerpolicy' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [// extraType
+                    '',
+                    'no-referrer',
+                    'no-referrer-when-downgrade',
+                    'same-origin',
+                    'origin',
+                    'strict-origin',
+                    'origin-when-cross-origin',
+                    'strict-origin-when-cross-origin',
+                    'unsafe-url',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'sizes' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'src' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'srcset' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'usemap' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'width' => [
+                XHPAttributeType::TYPE_INTEGER()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    protected string $tagName = 'img';
+}
