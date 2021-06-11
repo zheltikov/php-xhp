87d362c45ec84e6f74dab0846afdaa831e903dc7
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 16:53:32 +0300

    Html\Tags\Q

---
+++ src/Html/Tags/Q.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Core\ChildValidation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
+use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Palpable;
+use Zheltikov\PhpXhp\Html\Category\Phrase;
+use Zheltikov\PhpXhp\Html\Element;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+final class Q extends Element implements Phrase, Flow, Palpable
+{
+    use Validation;
+
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'cite' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    protected static function getChildrenDeclaration(): Constraint
+    {
+        return ChildValidation::any_number_of(
+            ChildValidation::any_of(
+                ChildValidation::pcdata(),
+                ChildValidation::of_type(Phrase::class)
+            )
+        );
+    }
+
+    protected string $tagName = 'q';
+}
