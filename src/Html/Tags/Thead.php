9cf1cb4315f9b28203da73863e777bb8e6b8de55
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 20:23:32 +0300

    Html\Tags\Thead

---
+++ src/Html/Tags/Thead.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Core\ChildValidation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
+use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Html\Element;
+
+final class Thead extends Element
+{
+    use Validation;
+
+    protected static function getChildrenDeclaration(): Constraint
+    {
+        return ChildValidation::any_number_of(
+            ChildValidation::of_type(Tr::class),
+        );
+    }
+
+    protected string $tagName = 'thead';
+}
