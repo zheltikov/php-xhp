14f661eba526f598def2eacaf057271ffe06a487
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 11:20:27 +0300

    Html\Tags\Iframe

---
+++ src/Html/Tags/Iframe.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Html\Category\Embedded;
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Interactive;
+use Zheltikov\PhpXhp\Html\Category\Palpable;
+use Zheltikov\PhpXhp\Html\Category\Phrase;
+use Zheltikov\PhpXhp\Html\PCDataElement;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+final class Iframe extends PCDataElement implements Phrase, Flow, Interactive, Embedded, Palpable
+{
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'allow' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'allowfullscreen' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'allowpaymentrequest' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'name' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'height' => [
+                XHPAttributeType::TYPE_INTEGER()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'referrerpolicy' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [// extraType
+                    '',
+                    'no-referrer',
+                    'no-referrer-when-downgrade',
+                    'same-origin',
+                    'origin',
+                    'strict-origin',
+                    'origin-when-cross-origin',
+                    'strict-origin-when-cross-origin',
+                    'unsafe-url',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'sandbox' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'seamless' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'src' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'srcdoc' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'width' => [
+                XHPAttributeType::TYPE_INTEGER()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    protected string $tagName = 'iframe';
+}
+
