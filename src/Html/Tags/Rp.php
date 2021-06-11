008b3001788ad41928741e7ae57155c13a99445f
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 16:59:05 +0300

    Html\Tags\Rp

---
+++ src/Html/Tags/Rp.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
+use Zheltikov\PhpXhp\Core\ChildValidation;
+use Zheltikov\PhpXhp\Html\Category\Phrase;
+use Zheltikov\PhpXhp\Html\Element;
+
+class Rp extends Element
+{
+    use Validation;
+
+    protected static function getChildrenDeclaration(): Constraint
+    {
+        return ChildValidation::at_least_one_of(
+            ChildValidation::any_of(
+                ChildValidation::pcdata(),
+                ChildValidation::of_type(Phrase::class),
+            )
+        );
+    }
+
+    protected string $tagName = 'rp';
+}
