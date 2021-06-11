e37e16bb696ec47111ca6929cda33eb3d22ad99e
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 20:12:40 +0300

    Html\Tags\Template

---
+++ src/Html/Tags/Template.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Metadata;
+use Zheltikov\PhpXhp\Html\Category\Phrase;
+use Zheltikov\PhpXhp\Html\Element;
+
+final class Template extends Element implements Phrase, Metadata, Flow
+{
+    // The children declaration for this element is extraordinarily verbose so
+    // I leave it to you to use it appropriately.
+
+    protected string $tagName = 'template';
+}
