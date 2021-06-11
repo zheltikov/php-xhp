a23781aeb208f302c72438b5bda457c4e1ccb6c7
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 15:39:37 +0300

    Html\Tags\Meta

---
+++ src/Html/Tags/Meta.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Metadata;
+use Zheltikov\PhpXhp\Html\Category\Phrase;
+use Zheltikov\PhpXhp\Html\Singleton;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+// If itemprop is present, this element is allowed within the <body>.
+final class Meta extends Singleton implements Phrase, Flow, Metadata
+{
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'charset' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'content' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+
+            // The correct definition of http-equiv is an enum, but there are
+            // legacy values still used and any strictness here would only be
+            // frustrating.
+            'http-equiv' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+
+            'name' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+
+            // Facebook OG
+            'property' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    protected string $tagName = 'meta';
+}
