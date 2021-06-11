07a8a9a93f162d1eaf5d8c3b99b6bdc09793710f
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 15:54:52 +0300

    Html\Tags\Noscript

---
+++ src/Html/Tags/Noscript.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Core\ChildValidation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
+use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Metadata;
+use Zheltikov\PhpXhp\Html\Category\Phrase;
+use Zheltikov\PhpXhp\Html\Element;
+
+final class Noscript extends Element implements Phrase, Metadata, Flow
+{
+    use Validation;
+
+    protected static function getChildrenDeclaration(): Constraint
+    {
+        return ChildValidation::any_number_of(
+            ChildValidation::any_of(
+                ChildValidation::pcdata(),
+                ChildValidation::of_type(Metadata::class),
+                ChildValidation::of_type(Flow::class),
+            ),
+        );
+    }
+
+    protected string $tagName = 'noscript';
+}
