60328e877a9cca06d6c7946ab332032c86eff341
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 17:27:15 +0300

    Html\Tags\Select

---
+++ src/Html/Tags/Select.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
+use Zheltikov\PhpXhp\Core\ChildValidation;
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Interactive;
+use Zheltikov\PhpXhp\Html\Category\Palpable;
+use Zheltikov\PhpXhp\Html\Category\Phrase;
+use Zheltikov\PhpXhp\Html\Element;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+final class Select extends Element implements Phrase, Flow, Interactive, Palpable
+{
+    use Validation;
+
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'autocomplete' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'disabled' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'form' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'multiple' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'name' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'required' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'size' => [
+                XHPAttributeType::TYPE_INTEGER()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    protected static function getChildrenDeclaration(): Constraint
+    {
+        return ChildValidation::any_number_of(
+            ChildValidation::any_of(
+                ChildValidation::of_type(Option::class),
+                ChildValidation::of_type(Optgroup::class),
+            )
+        );
+    }
+
+    protected string $tagName = 'select';
+}
