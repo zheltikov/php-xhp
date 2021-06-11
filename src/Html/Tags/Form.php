a42902efec49f301870ffab5b043e442a1d13477
Max Zheltikov <maxcruer@gmail.com>
2021-06-11 09:48:48 +0300

    Html\Tags\Form

---
+++ src/Html/Tags/Form.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Zheltikov\PhpXhp\Html\Tags;
+
+use Zheltikov\PhpXhp\Core\ChildValidation\Validation;
+use Zheltikov\PhpXhp\Core\ChildValidation\Constraint;
+use Zheltikov\PhpXhp\Core\ChildValidation;
+use Zheltikov\PhpXhp\Html\Category\Flow;
+use Zheltikov\PhpXhp\Html\Category\Palpable;
+use Zheltikov\PhpXhp\Html\Element;
+use Zheltikov\PhpXhp\Reflection\XHPAttributeType;
+
+final class Form extends Element implements Flow, Palpable
+{
+    use Validation;
+
+    protected static function __xhpAttributeDeclaration(): array
+    {
+        return [
+            'action' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'accept-charset' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'autocomplete' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'on',
+                    'off',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'enctype' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'method' => [
+                XHPAttributeType::TYPE_ENUM()->getValue(), // type
+                [ // extraType
+                    'get',
+                    'post',
+                ],
+                null, // defaultValue
+                false, // required
+            ],
+            'name' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'novalidate' => [
+                XHPAttributeType::TYPE_BOOL()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'rel' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+            'target' => [
+                XHPAttributeType::TYPE_STRING()->getValue(), // type
+                null, // extraType
+                null, // defaultValue
+                false, // required
+            ],
+        ];
+    }
+
+    // Should not contain :form
+    protected static function getChildrenDeclaration(): Constraint
+    {
+        return ChildValidation::any_number_of(
+            ChildValidation::any_of(
+                ChildValidation::pcdata(),
+                ChildValidation::of_type(Flow::class)
+            )
+        );
+    }
+
+    protected string $tagName = 'form';
+}
